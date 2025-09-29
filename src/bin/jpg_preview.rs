2de3aa1fbe76c5c73cdf473368b7466730e3b771
Sai Sasidhar Maddali <mail@sasidhar.in>
2025-09-28 17:15:13 -0700

    refactor: parameterize film base values so that they can be set by the caller

--- src/bin/jpg_preview.rs
+++ src/bin/jpg_preview.rs
@@ -1,11 +1,12 @@
 use image::{ImageReader, RgbImage};
-use ndarray::{Array3};
+use ndarray::Array3;
 use show_image::{ImageInfo, ImageView, create_window};
 
 use agitsu::filter::processing;
 
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let film_base: [f32; 3] = [0.6454267, 0.4690254, 0.35811886];
     let neg_scan: RgbImage = ImageReader::open("negative.jpg")?.decode()?.to_rgb8();
     let (width, height) = neg_scan.dimensions();
 
@@ -14,7 +15,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             neg_scan.as_raw()[(y * width as usize + x) * 3 + c] as f32 / 255.0
         });
 
-    let pos_image: Array3<f32> = processing::invert_color_negative(&neg_image.view());
+    let pos_image: Array3<f32> = processing::invert_color_negative(&neg_image.view(), film_base);
     let pos_preview: Array3<u8> = pos_image.map(|&x| (x.clamp(0.0, 1.0) * 255.0).round() as u8);
 
     let image_buffer: &[u8] = pos_preview.as_slice().unwrap();
--- src/filter/processing.rs
+++ src/filter/processing.rs
@@ -26,12 +26,12 @@ pub fn linear_to_srgb(x: &ArrayView3<f32>) -> Array3<f32> {
     })
 }
 
-pub fn invert(lin: &ArrayView3<f32>, film_base_estimate: [f32; 3]) -> Array3<f32> {
+pub fn invert(lin: &ArrayView3<f32>, film_base: [f32; 3]) -> Array3<f32> {
     let shape = lin.raw_dim();
     let mut inv = Array3::<f32>::zeros(shape);
 
     for ch in 0..3 {
-        let channel_film_base = film_base_estimate[ch];
+        let channel_film_base = film_base[ch];
         let channel = lin.index_axis(Axis(2), ch);
         let mut inv_channel = inv.index_axis_mut(Axis(2), ch);
 
@@ -41,10 +41,9 @@ pub fn invert(lin: &ArrayView3<f32>, film_base_estimate: [f32; 3]) -> Array3<f32
     inv
 }
 
-pub fn invert_color_negative(negative_image: &ArrayView3<f32>) -> Array3<f32> {
-    let film_base_estimate: [f32;3] = [0.6454267, 0.4690254, 0.35811886];
+pub fn invert_color_negative(negative_image: &ArrayView3<f32>, film_base: [f32;3]) -> Array3<f32> {
     let linear_negative_image: Array3<f32> = srgb_to_linear(&negative_image.view());
-    let linear_positive_image: Array3<f32> = invert(&linear_negative_image.view(), film_base_estimate);
+    let linear_positive_image: Array3<f32> = invert(&linear_negative_image.view(), film_base);
     linear_positive_image.map(|&x| (x.clamp(0.0, 1.0) * 255.0).round() as u8);
     let positive_image: Array3<f32> = linear_to_srgb(&linear_positive_image.view());
 
