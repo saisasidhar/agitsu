c20edc944ab7f2cfad746c3d2248bf906b8cf954
Sai Sasidhar Maddali <mail@sasidhar.in>
2025-09-28 17:56:44 -0700

    feat: use jpg film inversion fn in transform_frame()

--- src/filter/imp.rs
+++ src/filter/imp.rs
@@ -1,3 +1,4 @@
+use crate::filter::processing;
 use glib::subclass::object::ObjectImpl;
 use glib::subclass::prelude::ObjectSubclass;
 use gstreamer_base::gst::{
@@ -9,7 +10,7 @@ use gstreamer_base::subclass::prelude::{ElementImpl, GstObjectImpl};
 use gstreamer_base::{BaseTransform, gst};
 use gstreamer_video::subclass::prelude::VideoFilterImpl;
 use gstreamer_video::{VideoFilter, VideoFormat, VideoFrameExt, VideoFrameRef};
-use ndarray::{ArrayView3, ArrayViewMut3, Zip};
+use ndarray::{Array3, ArrayView3, ArrayViewMut3, Zip};
 use std::sync::LazyLock;
 
 #[derive(Default)]
@@ -75,6 +76,7 @@ impl VideoFilterImpl for AgitsuFilter {
         inframe: &VideoFrameRef<&BufferRef>,
         outframe: &mut VideoFrameRef<&mut BufferRef>,
     ) -> Result<FlowSuccess, FlowError> {
+        let film_base: [f32; 3] = [0.6454267, 0.4690254, 0.35811886];
         let info = inframe.info();
         let width = info.width() as usize;
         let height = info.height() as usize;
@@ -82,17 +84,14 @@ impl VideoFilterImpl for AgitsuFilter {
         let in_data = inframe.plane_data(0).unwrap();
         let out_data = outframe.plane_data_mut(0).unwrap();
 
-        let in_arr: ArrayView3<u8> =
-            ArrayView3::from_shape((height, width, 3), in_data).map_err(|_| FlowError::Error)?;
-        let mut out_arr: ArrayViewMut3<u8> =
-            ArrayViewMut3::from_shape((height, width, 3), out_data)
-                .map_err(|_| FlowError::Error)?;
+        let in_arr: Array3<f32> = Array3::from_shape_fn((height, width, 3), |(y, x, c)| {
+            in_data[(y * width + x) * 3 + c] as f32 / 255.0
+        });
+        let out_arr: Array3<f32> = processing::invert(&in_arr.view(), film_base);
 
-        Zip::from(&mut out_arr)
-            .and(&in_arr)
-            .for_each(|out_px, &in_px| {
-                *out_px = 255u8.saturating_sub(in_px);
-            });
+        for (out_px, &val) in out_data.iter_mut().zip(out_arr.iter()) {
+            *out_px = (val.clamp(0.0, 1.0) * 255.0).round() as u8;
+        }
 
         Ok(FlowSuccess::Ok)
     }
